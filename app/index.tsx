28efac6167c8faa05799f4455959b08e6c89e91a
bilalparray <parraybilal34@gmail.com>
2024-08-03 21:47:14 +0530

    modal

--- app/index.tsx
+++ app/index.tsx
@@ -7,6 +7,8 @@ import {
   Text,
   View,
   Dimensions,
+  BackHandler,
+  Alert,
 } from "react-native";
 import CustomInput from "./Input";
 import Dropdown from "./Dropdown";
@@ -23,16 +25,18 @@ import {
   TestIds,
   useForeground,
 } from "react-native-google-mobile-ads";
-const adUnitId = "ca-app-pub-3821692834936093/3250384525";
-
+import Modal from "react-native-modal";
+import { environment } from "@/environment";
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
 
 const CompoundingCalculator = () => {
+  const [isModalVisible, setModalVisible] = useState(false);
   const bannerRef = useRef<BannerAd>(null);
   useForeground(() => {
     Platform.OS === "ios" && bannerRef.current?.load();
   });
+
   const [principalAmount, setPrincipalAmount] = useState("");
   const [time, setTime] = useState("");
   const [percent, setPercent] = useState("");
@@ -50,6 +54,18 @@ const CompoundingCalculator = () => {
     if (bannerRef.current) {
       bannerRef.current.load();
     }
+
+    const backAction = () => {
+      setModalVisible(true);
+      return true; // Prevent default behavior
+    };
+
+    const backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      backAction
+    );
+
+    return () => backHandler.remove();
   }, []);
 
   const handlePrincipalAmount = (text: string) => {
@@ -153,75 +169,102 @@ const CompoundingCalculator = () => {
     { label: "Daily", value: TimePeriod.Days },
   ];
 
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
+
+  const exitApp = () => {
+    BackHandler.exitApp();
+    toggleModal();
+  };
+
   return (
-    <ScrollView>
-      <SafeAreaView style={styles.safeArea}>
-        <View style={[styles.titleContainer, { height: windowHeight }]}>
-          <Text style={styles.title}>Compounding Calculator</Text>
-          <CustomInput
-            label="Enter Principal Amount"
-            placeholder="Enter Principal Amount"
-            type="number"
-            icon="numbers"
-            value={principalAmount}
-            onChangeText={handlePrincipalAmount}
-            error={numberError}
-          />
-          <CustomInput
-            label="Enter Rate of Interest"
-            placeholder="Enter Rate of Interest"
-            type="number"
-            icon="percent"
-            value={percent}
-            onChangeText={handlePercentChange}
-            error={percentError}
-          />
-          <Dropdown
-            label="Choose Compounding Frequency"
-            selectedValue={selectedFrequency}
-            onValueChange={handleCompoundingFrequencyChange}
-            items={compoundingFrequencyItems}
-          />
-          <CustomInput
-            label="Enter Time"
-            placeholder="Enter Time"
-            type="number"
-            icon="numbers"
-            value={time}
-            onChangeText={handleTime}
-            error={timeError}
-          />
-          <Dropdown
-            label="Choose Time Unit"
-            selectedValue={selectedTime}
-            onValueChange={handleTimeChange}
-            items={timeItems}
-          />
-          <View style={styles.buttonContainer}>
-            <MyButton onPress={clearInputFields} text="Clear" />
-            <MyButton onPress={handleButtonClick} text="Calculate" />
-          </View>
-          {/* Conditional rendering of Card */}
-          {result !== "" && (
-            <Card
-              totalAmount={Number(principalAmount) + Number(result)}
-              principalAmount={principalAmount}
-              interestAmount={result}
+    <>
+      <ScrollView>
+        <SafeAreaView style={styles.safeArea}>
+          <View style={[styles.titleContainer, { height: windowHeight }]}>
+            <BannerAd
+              ref={bannerRef}
+              unitId={environment.adUnitId}
+              size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
             />
-          )}
+            <Text style={styles.title}>Compounding Calculator</Text>
+            <CustomInput
+              label="Enter Principal Amount"
+              placeholder="Enter Principal Amount"
+              type="number"
+              icon="numbers"
+              value={principalAmount}
+              onChangeText={handlePrincipalAmount}
+              error={numberError}
+            />
+            <CustomInput
+              label="Enter Rate of Interest"
+              placeholder="Enter Rate of Interest"
+              type="number"
+              icon="percent"
+              value={percent}
+              onChangeText={handlePercentChange}
+              error={percentError}
+            />
+            <Dropdown
+              label="Choose Compounding Frequency"
+              selectedValue={selectedFrequency}
+              onValueChange={handleCompoundingFrequencyChange}
+              items={compoundingFrequencyItems}
+            />
+            <CustomInput
+              label="Enter Time"
+              placeholder="Enter Time"
+              type="number"
+              icon="numbers"
+              value={time}
+              onChangeText={handleTime}
+              error={timeError}
+            />
+            <Dropdown
+              label="Choose Time Unit"
+              selectedValue={selectedTime}
+              onValueChange={handleTimeChange}
+              items={timeItems}
+            />
+            <View style={styles.buttonContainer}>
+              <MyButton onPress={clearInputFields} text="Clear" />
+              <MyButton onPress={handleButtonClick} text="Calculate" />
+            </View>
+            {/* Conditional rendering of Card */}
+            {result !== "" && (
+              <Card
+                totalAmount={Number(principalAmount) + Number(result)}
+                principalAmount={principalAmount}
+                interestAmount={result}
+              />
+            )}
+            <BannerAd
+              ref={bannerRef}
+              unitId={environment.adUnitId}
+              size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
+            />
+          </View>
+        </SafeAreaView>
+      </ScrollView>
+
+      {/* Custom Modal for Exit Confirmation */}
+      <Modal isVisible={isModalVisible} style={styles.modal}>
+        <View style={styles.modalContent}>
           <BannerAd
             ref={bannerRef}
-            unitId={adUnitId}
+            unitId={environment.adUnitId}
             size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
           />
-          <BannerAd
-            ref={bannerRef}
-            unitId={adUnitId}
-            size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
-          />
+          <Text style={styles.modalText}>Are you sure you want to exit?</Text>
+          <View style={styles.modalButtons}>
+            <MyButton onPress={toggleModal} text="Cancel" />
+            <MyButton onPress={exitApp} text="OK" />
+          </View>
         </View>
-      </SafeAreaView>
-    </ScrollView>
+      </Modal>
+    </>
   );
 };
 
@@ -247,6 +290,29 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-around",
     marginVertical: 8,
+    width: "100%",
+  },
+  modal: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalContent: {
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 10,
+    alignItems: "center",
+    width: windowWidth - 40,
+  },
+  modalText: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  modalAd: {
+    marginVertical: 10,
+  },
+  modalButtons: {
+    flexDirection: "row",
+    justifyContent: "space-around",
     width: "100%",
   },
 });
---
+++ environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  adUnitId: "ca-app-pub-3821692834936093/3250384525",
+};
